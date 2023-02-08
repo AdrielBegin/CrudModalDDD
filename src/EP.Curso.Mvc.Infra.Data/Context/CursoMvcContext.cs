3a1baa41c539556499f2f4dcbd5803743604e0af
Adriel Pereira <adriel.pemi@gmail>
2023-02-08 17:42:41 -0400

    Estrutura DB

---
+++ src/EP.Curso.Mvc.Infra.Data/Context/CursoMvcContext.cs
@@ -0,0 +1,39 @@
+﻿using EP.Curso.Mvc.Infra.Data.Mappings;
+using EP.CursoMvc.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.Curso.Mvc.Infra.Data.Context
+{
+    public class CursoMvcContext: DbContext
+    {
+        public DbSet<Cliente> Clientes{ get; set; }
+        public DbSet<Endereco> Enderecos{ get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            //modelBuilder.HasDefaultSchema("Sistema1");
+
+            modelBuilder.Configurations.Add(new ClienteMapping());
+            modelBuilder.Configurations.Add(new EnderecoMapping());
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ src/EP.Curso.Mvc.Infra.Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,45 @@
+﻿using EP.CursoMvc.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EP.Curso.Mvc.Infra.Data.Mappings
+{
+    public  class EnderecoMapping : EntityTypeConfiguration<Endereco>
+    {
+        public EnderecoMapping() 
+        {
+            HasKey(e => e.Id);
+
+            Property( e => e.Logradouro)
+                .IsRequired()
+                .HasMaxLength(50);
+            
+            Property( e => e.Numero)
+                .IsRequired() 
+                .HasMaxLength(20);
+
+            Property( e => e.Bairro)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            Property( e => e.CEP)
+                .IsRequired()
+                .HasMaxLength (8)
+                .IsFixedLength();
+
+            Property( e => e.Complemento)                
+                .HasMaxLength(100);
+            //HasOptional
+            HasRequired(c => c.Cliente)
+                .WithMany(c => c.Endereco)
+                .HasForeignKey(e => e.ClienteId);
+
+            ToTable("Endereco");
+        }
+
+    }
+}
