34b34114993637a3437a2152364ceb9648a217c0
Adriel Pereira <adriel.pemi@gmail>
2023-02-08 10:23:07 -0400

    Cria tabela

--- src/EP.CursoMvc.Domain/Models/Cliente.cs
+++ src/EP.CursoMvc.Domain/Models/Cliente.cs
@@ -8,13 +8,24 @@ namespace EP.CursoMvc.Domain.Models
 {
     public class Cliente : Entity
     {        
+        public string Nome { get; set; }
         public string Email { get; set; }
         public string CPF{ get; set; }
         public DateTime DataNascimento { get; set; }
         public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set;}
         public bool Excluido { get; set;}
+
+        public ICollection<Endereco> Endereco { get; set; } 
         
+        public void  AdicionarEndereco (Endereco endereco)
+        {
+            if (!endereco.EhValido()) 
+            {
+                ValidationResult = endereco.ValidationResult; 
+                return;
+            }
+        }
         public void DefinirComoExcluido() 
         { 
             Ativo = false;
@@ -24,6 +35,16 @@ namespace EP.CursoMvc.Domain.Models
         {
             Ativo = true;
             Excluido = false;
+        }
+
+        public override bool EhValido()
+        {
+            if (string.IsNullOrWhiteSpace(Nome))
+                AdicionarErroValidacao("Nome", "O campo não pode estar vazio!");
+            if (string.IsNullOrWhiteSpace(Email))
+                AdicionarErroValidacao("Email", "O campo não pode estar vazio!");
+
+            return ValidationResult.Count == 0; 
         }
     }
 }
--- src/EP.CursoMvc.Domain/Models/Entity.cs
+++ src/EP.CursoMvc.Domain/Models/Entity.cs
@@ -10,8 +10,24 @@ namespace EP.CursoMvc.Domain.Models
     {
         protected Entity()
         {
-            Id  = Guid.NewGuid();
+            Id = Guid.NewGuid();
+            ValidationResult = new Dictionary<string, string>();
         }
         public Guid Id { get; set; }
+
+        public IDictionary<string, string> ValidationResult { get; set; }
+
+        public void AdicionarErroValidacao( string erro, string mensagem)
+        {
+            ValidationResult.Add(erro, mensagem);   
+        }
+
+        public void ZerarlistaErros() 
+        {
+            ValidationResult = new Dictionary<string, string>();
+        }
+
+        public abstract bool EhValido();
     }
 }
+ 
